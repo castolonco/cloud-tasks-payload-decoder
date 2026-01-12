83cd55ac578f0041e43d3880e4923614fb7a82ba
Michael Lovitt <lovitt@castolon.co>
2026-01-12 15:28:48 -0600

    Migrate to ESLint 9 flat config (#3)

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        chrome: 'readonly',
+        CloudTasksDecoder: 'readonly',
+        CloudTasksDomHelpers: 'readonly'
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+    }
+  }
+];
